2edada38a2a162a48970d163db572d07de9f7d15
Elliot SecOps <elliotsecops@protonmail.com>
2024-07-12 21:10:12 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="PortSeeker",
+    version="0.1.0",
+    author="ElliotSecOps",
+    author_email="elliotsecops@protonmail.com",
+    description="A port scanning and vulnerability assessment tool leveraging the NVD API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DigitalNinja00/PortSeeker",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests>=2.25.1",
+        "python-dotenv>=0.15.0",
+        "requests-cache>=0.6.0",
+        "tenacity>=7.0.0",
+    ],
+    extras_require={
+        'dev': ['pytest>=6.2.3', 'flake8>=3.9.0'],
+    },
+    package_data={
+        'portseeker': ['data/*.json', 'config/*.yaml'],
+    },
+    entry_points={
+        'console_scripts': [
+            'portseeker=portseeker:main',
+        ],
+    },
+)
